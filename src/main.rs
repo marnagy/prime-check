0387e8a056f606343d0cf6803127e9a7ea0f59d4
Marek Nagy <62247071@cuni.cz>
2020-07-28 23:49:41 +0200

    Made prime number checker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+     match io::stdin().read_line(&mut input) {
+        Ok(_) =>{
+            match input.trim().parse::<i64>() {
+                Ok(number) => {
+                    let mut is_prime : bool = true;
+                    let mut divisor : i64 = -1;
+                    println!("Sqrt of {0} is {1}.", number, (number as f32).sqrt() as i64);
+                    for n in 2..((number as f64).sqrt() as i64 -1) {
+                        if number % n == 0  {
+                            is_prime = false;
+                            divisor = n;
+                            break;
+                        }
+                    }
+                    if is_prime {
+                        println!("{0} is a prime.", number);
+                    }
+                    else{
+                        println!("{0} is NOT a prime.", number);
+                        println!("Divisible by {0}.", divisor);
+                    }
+                }
+                Err(_) => println!("Not a valid number.")
+            }
+        }
+        Err(_) => println!("No input detected.")
+     }
 }
