f0de36af94dfb3431ccb5c31149d9e9283b51637
Marek Nagy <62247071@cuni.cz>
2020-08-05 10:41:27 +0200

    Reformatting

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,15 @@ fn main() {
     match io::stdin().read_line(&mut input) {
         Ok(_) => match input.trim().parse::<i64>() {
             Ok(number) => {
-                is_prime(number);
+                match is_prime(number) {
+                    Ok(res) => if res{
+                        println!("Given number is a prime.");
+                    }
+                    else{
+                        println!("Given number is not a prime.");
+                    },
+                    Err(reason) => print!("{}", reason)
+                }
             }
             Err(_) => println!("Not a valid number."),
         },
@@ -13,21 +21,19 @@ fn main() {
     }
 }
 
-fn is_prime(number : i64) -> Result<bool, String> {
+fn is_prime(number: i64) -> Result<bool, String> {
     if number >= 2 {
         let mut is_prime = true;
-        let mut divisor: i64 = -1;
         println!("Sqrt of {0} is {1}.", number, (number as f32).sqrt());
         //println!("Sqrt of {0} is {1}.", number, (number as f32).sqrt() as i32);
         for n in 2..((number as f64).sqrt().floor() as i64 + 1) {
             if number % n == 0 {
                 is_prime = false;
-                divisor = n;
                 break;
             }
         }
         return Result::Ok(is_prime);
     } else {
-        return Result::Err( String::from("Not a valid number"));
+        return Result::Err(String::from("The number is too small."));
     }
 }
